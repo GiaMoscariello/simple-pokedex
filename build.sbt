2a73c9fd857c7b933299dec3052b7f8f70b21144
GiaMoscariello <gia.moscariello@gmail.com>
2022-11-26 14:29:31 +0100

    fix Dependencies

--- build.sbt
+++ build.sbt
@@ -1,15 +1,15 @@
-scalaVersion := "2.13.6"
+scalaVersion := "2.13.8"
 resolvers += "Confluent Repo" at "https://packages.confluent.io/maven"
 
 name := "pokemon-api"
 organization := "gia.moscariello"
 
-scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Ywarn-dead-code")
-
-crossScalaVersions := Seq("2.13.6", "3.2.1")
+scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Ywarn-dead-code", "-Ymacro-annotations")
 
 libraryDependencies ++=
+    Seq(Dependencies.cacheDependencies) ++
     Dependencies.testDependencies ++
     Dependencies.loggingDependencies ++
     Dependencies.circeDependencies ++
-    Dependencies.http4sDependencies
+    Dependencies.http4sDependencies ++
+    Dependencies.sttpDependencies
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,39 +1,50 @@
 import sbt._
 
+
 object Dependencies {
-  val DoobieVersion = "1.0.0-RC1"
-  val NewTypeVersion = "0.4.4"
 
-  val Log4jVersion = "2.17.1"
+  val scalaVersion = "2.13.8"
+  val Log4jVersion = "2.17.2"
+  val CirceVersion = "0.14.2"
+  val SttpVersion = "3.6.2"
+  val TestContainersVersion = "0.40.8"
+  val MockServerJavaVersion = "5.13.2"
+  val CatsEffectVersion = "3.3.12"
 
-  val Http4sVersion = "1.0.0-M21"
-  val CirceVersion = "0.15.0-M1"
+  val Http4sVersion = "0.23.0"
 
-  val http4sDependencies: Seq[ModuleID] = Seq(
-    "org.http4s" %% "http4s-blaze-server" % Http4sVersion,
-    "org.http4s" %% "http4s-circe" % Http4sVersion,
-    "org.http4s" %% "http4s-dsl" % Http4sVersion,
-    "io.github.juliano" % "pokeapi-scala_3" % "0.1.0",
-
-  )
+  val catsDependencies: ModuleID = "org.typelevel" %% "cats-effect" % CatsEffectVersion
+  val scalaXmlDeps = "org.scala-lang.modules" %% "scala-xml" % "2.1.0"
 
   val circeDependencies: Seq[ModuleID] = Seq(
     "io.circe" %% "circe-core" % CirceVersion,
     "io.circe" %% "circe-generic" % CirceVersion,
     "io.circe" %% "circe-parser" % CirceVersion,
-    ("io.circe" %% "circe-generic-extras" % "0.14.3"),
+    "io.circe" %% "circe-generic-extras" % CirceVersion,
+  )
+
+  val http4sDependencies: Seq[ModuleID] = Seq(
+    "org.http4s" %% "http4s-blaze-server",
+    "org.http4s" %% "http4s-circe",
+    "org.http4s" %% "http4s-dsl"
+  ).map(_ % Http4sVersion)
+
+
+  val testDependencies: Seq[ModuleID] = Seq(
+    "org.scalatest" %% "scalatest" % "3.2.12" % Test,
+    "com.github.tomakehurst" % "wiremock-jre8" % "2.35.0" % Test
   )
 
   val loggingDependencies: Seq[ModuleID] = Seq(
-    "org.typelevel" %% "log4cats-slf4j" % "2.2.0",
-    "ch.qos.logback" % "logback-classic" % "1.2.10",
-    "org.slf4j" % "slf4j-simple" % "1.7.36"
+    "org.typelevel" %% "log4cats-slf4j" % "2.3.0",
+    "ch.qos.logback" % "logback-classic" % "1.2.11"
   )
 
-  val testDependencies: Seq[ModuleID] = Seq(
-    "org.scalatest" %% "scalatest" % "3.2.11" ,
-    "com.dimafeng" %% "testcontainers-scala" % "0.40.0" ,
-    "com.dimafeng" %% "testcontainers-scala-postgresql" % "0.40.0"
-  )
+  val sttpDependencies: Seq[ModuleID] = Seq(
+    "com.softwaremill.sttp.client3" %% "core",
+    "com.softwaremill.sttp.client3" %% "async-http-client-backend-cats",
+  ).map(_ % SttpVersion)
 
+  val cacheDependencies =
+    "com.github.blemale" %% "scaffeine" % "5.1.2"
 }
